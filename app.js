c14463e29e42201a8cc479908bfc3da7965194c6
ianbett95 <ianbett95@gmail.com>
2022-08-17 13:24:20 +0300

    added db and connection queries

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import mysql from "mysql"
 
 const app = express();
+ 
+const connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'users'
+})
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-const users = [
-  {
-    name: "jina la mtu",
-    email: "jiia@gmail.com",
-    password: "password",
-  },
-
-  {
-    name: "jina la mtu",
-    email: "jiia@gmail.com",
-    password: "password",
-  },
-
-  {
-    name: "toddler",
-    email: "tod@gmail.com",
-    password: "$2b$10$aXeP3DGib.0qM.dXYk98BuLNf80hWVhQBmPrzdfvRAmS7Zy1F8QrK",
-  },
-  {
-    name: "user",
-    email: "user@test.com",
-    password: "$2b$10$g6Mpv5XrJ3mNFG6ZmCs3beVVUb7YIbE63ztrwyKkZij35qE.thW7G"
-  
-  },
-];
 //display signup page
 
 app.get("/signup", (req, res) => {
@@ -55,21 +38,40 @@ app.post("/signup", (req, res) => {
   };
   if (user.password === user.confirmPassword) {
     //check if user exists
-    let userExists = users.find((account) => account.email === user.email);
+     
+    let sql = 'SELECT * FROM student WHERE email = ?'
+    connection.query(
+      sql,[user.email],(error,results)=>{
+        if(results.length>0){
+           let message = "account already exists with the email provided";
+           res.render("signup", { error: true, message: message, user: user });
+        }else{
+          bcrypt.hash(user.password,10,(error,hash)=>{
+            let sql = 'INSERT INTO student(email,name,password) VALUES(?,?,?)'
+            connection.query(
+              sql,
+              [
+                user.email,
+                user.name,
+                hash
+              ],
+              (error,results)=>{
+                res.send('account successfully created')
+              }
+              
+            )
+          })
+        }
+      }
+    )
+  
 
-    if (userExists) {
-      let message = "account already exists with the email provided";
-      res.render("signup", { error: true, message: message, user: user });
+    
     } else {
-      //create account
-      bcrypt.hash(user.password, 10, (error, hash) => {
-        (user.password = hash), res.send(user);
-      });
-    }
-  } else {
-    let message = "password/confirm password mismatch";
-    res.render("signup", { error: true, message: message, user: user });
-  }
+       let message = "password/confirm password mismatch";
+       res.render("signup", { error: true, message: message, user: user});
+    }  
+    
 });
 
 //landing page
@@ -92,21 +94,32 @@ app.post("/login", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   };
+
+  let sql='SELECT* FROM student WHERE email=?'
+  connection.query(
+   sql,[user.email], 
+   (error,results) => {
+    if (results.length>0) {
+      bcrypt.compare(
+        user.password,
+        results[0].password,
+        (error, passwordMatches) => {
+          if (passwordMatches) {
+            res.send("grant access");
+          } else {
+            let message = "Incorrect password";
+            res.render("login", { error: true, message: message, user: user });
+          }
+        }
+      );
+    } else {
+      let message = "account does not exist please create one";
+      res.render("login", { error: true, message: message, user: user });
+    }
+   }
+  )
   // validation
-  let userExists = users.find((account) => account.email === user.email);
-  if (userExists) {
-    bcrypt.compare(user.password,userExists.password, (error,passwordMatches)=>{
-      if(passwordMatches){
-        res.send('grant access')
-      }else{
-        let message ='Incorrect password'
-        res.render('login',{error:true,message:message,user:user})
-      }
-    })
-  } else {
-    let message = "account does not exist please create one";
-    res.render("login", { error: true, message: message, user: user });
-  }
+  
 });
 
 const PORT = process.env.PORT || 4000;
