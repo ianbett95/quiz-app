f05a2a13d44c8a33f2ec233fba9c48dcfd35fe6c
ianbett95 <ianbett95@gmail.com>
2022-08-26 12:32:08 +0300

    created  check score

--- app.js
+++ app.js
@@ -114,6 +114,26 @@ app.get('/dashboard', (req, res) => {
 app.get('/quiz',(req,res)=>{
   res.render('quiz')
 })
+
+app.post('/quiz',(req,res)=>{
+  const choices =[]
+  const answers = req.body.markingScheme.split(',')
+  for(let i=1;i<=10;i++){
+    let choice={
+      id:1,
+      yourAnswer:req.body[`q${i}`],
+      correctAnswer:answers[i-1],
+      score:0
+    }
+     //choices.push(req.body[`q${i}`])
+     if (choice.yourAnswer===choice.correctAnswer) {
+     choice.score=1
+     }
+    choices.push(choice)
+  }
+  console.log(choices)
+  console.log(`you scored ${choices.map(choice=>choice.score).reduce((a,b)=>a+b)}`)
+})
 //profile
 app.get('/profile', (req, res) => {
   if (res.locals.isLoggedIn) {
