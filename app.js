74821af04db8bb6478b5893e1173c71028b088f1
ianbett95 <ianbett95@gmail.com>
2022-09-01 11:48:37 +0300

    created admin-panel route

--- app.js
+++ app.js
@@ -110,6 +110,16 @@ app.get('/dashboard', (req, res) => {
     res.redirect('/login')
   }
 })
+//admin-panel
+app.get('/admin',(req,res)=>{
+   let sql='SELECT s_id,name,gender,picture,results FROM student JOIN score ON s_id=s_id_fk'
+  connection.query(
+    sql,(error,results)=>{
+      res.render('admin-panel',{students:results})
+      console.log(results)
+    }
+  )
+})
 //results
 app.get('/results',(req,res)=>{
   if (res.locals.isLoggedIn) {
